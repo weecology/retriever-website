4e9e23ca9e76bec2a35425495f85a2ea9f7cc915
Ethan White <ethan@weecology.org>
2013-12-10 22:20:18 -0500

    Make changes for deploying to GitHub pages

--- config.rb
+++ config.rb
@@ -4,11 +4,11 @@ require 'sass-globbing'
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
+http_path = "/retriever-website/"
+http_images_path = "/retriever-website/images"
 http_generated_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_fonts_path = "/retriever-website/fonts"
+css_dir = "public/retriever-website/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
